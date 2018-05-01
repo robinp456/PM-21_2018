e59167e9e5bb9ebd00735b3f638d37df1b4af8b9
robinp456 <38693121+robinp456@users.noreply.github.com>
2018-05-01 19:50:09 +0200

    Update DNA_Analyse.py

--- dna_analyse/DNA_Analyse.py
+++ dna_analyse/DNA_Analyse.py
@@ -12,7 +12,6 @@ mw_oh = 17010
 
 def hole_dna_sequenz():
     """Liefere eine vom Nutzer erfragte und validierte DNA-Sequenz.
-
     Um gültig zu sein, darf die Sequenz nur aus den Buchstaben
     A, G, T, C, a, g, t, c bestehen.
     """
@@ -29,7 +28,14 @@ def hole_dna_sequenz():
         # der Eingabe auf Gültigkeit geprüft werden muss.
         # Für einen Anfänger ist das schon eine ordentlich harte Nuss,
         # die aber mit etwas Anstrengung zu knacken sein sollte.
-
+        for nucleotid in sequence:
+            if nucleotid in gueltige_nukleotide:
+                erfolgreiche_validierung= True
+            elif nucleotid in [x.lower() for x in gueltige_nukleotide]:
+                erfolgreiche_validierung= True
+            else:
+                erfolgreiche_validierung = False
+                break
         if erfolgreiche_validierung:
             break
         else:
@@ -45,10 +51,35 @@ def hole_dna_sequenz():
         
     return sequence
 
+def evaluate_sequence(sequence):
+    a_count = 0
+    c_count = 0
+    g_count = 0
+    t_count = 0
+    for nucleotide in sequence:
+        if nucleotide == 'A' or nucleotide == 'a':
+            a_count = a_count + 1
+        if nucleotide == 'C' or nucleotide == 'c':
+            c_count = c_count + 1
+        if nucleotide == 'G' or nucleotide == 'g':
+            g_count = g_count + 1
+        if nucleotide == 'T' or nucleotide == 't':
+            t_count = t_count + 1
+    print('Länge:\t',len(sequence))
+    print('Base\\tHäufigkeit')
+    print('A\t', a_count)
+    print('C\t', c_count)
+    print('G\t', g_count)
+    print('T\t', t_count)
+    print('%GC-Gehalt:\t', (c_count + g_count) / len(sequence))
+    weigth = a_count*mw_a + g_count*mw_g + c_count*mw_c + t_count*mw_t + mw_oh
+    print('Molekulargewicht:\t', weigth * 100)
 
 seq = hole_dna_sequenz()
+
 # Hier sollten jetzt die nötigen Berechnungen durchgeführt werden.
 
 # Und hier erfolgt dann die Ausgabe.
 print()
-print('Eingelesene Sequenz:')
+print('Eingelesene Sequenz: ', seq)
+evaluate_sequence(seq)

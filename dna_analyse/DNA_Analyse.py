680f2a2baa18f3cf5c0b77d3e60b27cfa2f25a54
robinp456 <38693121+robinp456@users.noreply.github.com>
2018-05-15 23:12:23 +0200

    Update DNA Analyse

--- dna_analyse/DNA_Analyse.py
+++ dna_analyse/DNA_Analyse.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+
+from collections import Counter
 gueltige_nukleotide = ['A', 'T', 'G', 'C']
 
 # Molekulargewichte in mg/mol
@@ -10,45 +15,93 @@ mw_c = 289180
 mw_oh = 17010
 
 
-def hole_dna_sequenz():
-    """Liefere eine vom Nutzer erfragte und validierte DNA-Sequenz.
-
-    Um gültig zu sein, darf die Sequenz nur aus den Buchstaben
-    A, G, T, C, a, g, t, c bestehen.
+def ist_valide_sequenz(seq, alphabet, match_case=False):
+    """Prüfe, ob eine Sequenz nur Buchstaben des erlaubten Alphabets enthält.
+    Als Alphabet kann als iterierbare Buchstabenfolge beliebigen Typs
+    übergeben werden.
+    Mit match_case=True muss die Groß-/Kleinschreibung der des Alphabets
+    entsprechen.
     """
 
-    # Hinweis: Verwende eine while-Schleife, innerhalb derer Du eine
-    # Sequenz vom Nutzer erfragst und die Eingabe validierst.
-    # Nur wenn die Validierung klappt, brichst Du die Schleife ab
-    # und lieferst die Sequenz zurück.
+    if match_case == False:
+        # Groß-/Kleinschreibung soll keine Rolle spielen.
+        # Deshalb wandeln wir die Sequenz und das Alphabet in Großbuchstaben
+        # um. Für das Alphabet ist das am einfachsten, wenn wir es zuerst
+        # in einen string verwandeln.
+        seq = seq.upper()
+        alphabet = ''.join(alphabet).upper()
+    for c in seq:
+        if c not in alphabet:
+            return False
+    return True
+
+
+def hole_dna_sequenz(erlaubte_nukleotide):
+    """Liefere eine vom Nutzer erfragte und validierte DNA-Sequenz.
+    Um gültig zu sein, darf die Sequenz nur aus den Buchstaben
+    aus erlaubte_nukleotide (in Groß- oder Kleinschreibung) bestehen.
+    """
+
     while True:
         sequence = input('Gib eine DNA-Sequenz ein: ')
-        
-        # Hier fehlt natürlich noch die Validierung.
-        # Dafür brauchst Du noch eine Schleife, in der jeder Buchstabe
-        # der Eingabe auf Gültigkeit geprüft werden muss.
-        # Für einen Anfänger ist das schon eine ordentlich harte Nuss,
-        # die aber mit etwas Anstrengung zu knacken sein sollte.
 
-        if erfolgreiche_validierung:
-            break
-        else:
-            # Dieser Teil wird ausgeführt wenn die Bedingung hinter if
-            # nicht erfüllt war.
-            print()
-            print(
-                'Bei der Eingabe handelt es sich nicht um eine gültige '
-                'DNA-Sequenz!'
-                )
-            print()
-            print()
-        
-    return sequence
+        if ist_valide_sequenz(sequence, erlaubte_nukleotide):
+            return sequence
+
+        # Dieser Teil wird ausgeführt wenn die Bedingung hinter if
+        # nicht erfüllt war.
+        print()
+        print(
+            'Bei der Eingabe handelt es sich nicht um eine gültige '
+            'DNA-Sequenz!'
+            )
+        print()
+        print()
 
 
-seq = hole_dna_sequenz()
-# Hier sollten jetzt die nötigen Berechnungen durchgeführt werden.
 
-# Und hier erfolgt dann die Ausgabe.
-print()
-print('Eingelesene Sequenz:')
+if __name__ == '__main__':
+    # Sequenz einlesen
+    # ----------------
+    seq = hole_dna_sequenz(gueltige_nukleotide)
+
+    # Berechnungen
+    # ------------
+
+    # Sequenzlänge
+    seqlen = len(seq)
+    # Basenhäufigkeiten
+    seq_dict = Counter(seq.upper())
+    
+    # GC-Gehalt
+    gc_gehalt = 100 * (seq_dict['G']+seq_dict['C']) / seqlen
+    # Molekulargewicht
+    mw_gesamt = seq_dict['G']*mw_g + seq_dict['A']*mw_a + seq_dict['T']*mw_t + seq_dict['C']*mw_c + mw_oh  # in mg/mol
+
+    complement = dict([('A', 'T'),('T', 'A'),('C', 'G'),('G', 'C')])
+    compl_seq = ""
+    for nucleotid in seq:
+        compl_seq += complement[nucleotid.upper()]
+    
+    reversed_compl_seq = compl_seq[::-1]
+
+    # Ausgabe
+    # -------
+    print()
+    print('Eingelesene Sequenz:', seq)
+    print()
+    print('Länge:', seqlen)
+    print()
+    print('Base\tHäufigkeit')
+    print('G', seq_dict['G'], sep='\t')
+    print('A', seq_dict['A'], sep='\t')
+    print('T', seq_dict['T'], sep='\t')
+    print('C', seq_dict['C'], sep='\t')
+    print()
+    print('% GC-Gehalt:', gc_gehalt)
+    print()
+    print("Komplementärsequenz:", compl_seq, sep='\t')
+    print()
+    print("reverse Komplementärsequenz:", reversed_compl_seq, sep='\t')
+    print()
+    print('Molekulargewicht:', mw_gesamt/1000, 'g/mol')
